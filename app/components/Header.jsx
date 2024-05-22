7eef439d37bfebdcf2f3b501b9df08e9e8db6e00
Abdallah BENJRADA <abdallah.benjrada@gmail.com>
2024-05-22 20:10:02 +0200

    Initialize Remix project

---
+++ app/components/Header.jsx
@@ -0,0 +1,16 @@
+// app/components/Header.jsx
+import { Link } from "@remix-run/react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+         
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,22 @@
+// app/routes/index.jsx
+import Layout from '../components/Layout';
 import type { MetaFunction } from "@remix-run/node";
+
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "FitTracker App" },
+    { name: "description", content: "Welcome to FitTracker!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Layout>
+      <div>
+        <h1>Welcome to FitTracker</h1>
+        <p>Track your fitness journey effortlessly.</p>
+      </div>
+    </Layout>
   );
 }
